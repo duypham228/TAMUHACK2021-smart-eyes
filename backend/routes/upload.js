cdeb171a4a9e086e63a4b2fa96f0d2afb4949d97
duypham228 <duy.cs.pham@gmail.com>
2021-01-30 18:13:17 -0600

    connect front and back, use filesystem for encoding face

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -4,7 +4,7 @@ const path = require("path");
 const multer = require("multer");
 
 const storage = multer.diskStorage({
-  destination: "./backend/known/",
+  destination: "./known/",
   filename: function (req, file, cb) {
     cb(null, "IMAGE-" + Date.now() + path.extname(file.originalname));
   },
